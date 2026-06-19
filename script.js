15b0a9f863ace6939094cefa7a45229745862e9e
SelfMimesis <diegoperezobrero@gmail.com>
2026-06-19 12:10:47 +0200

    ventana_internet

--- script.js
+++ script.js
@@ -170,6 +170,160 @@
     }, 15000);
   }
 
+  function getBrowserTitle() {
+    if (document.body.classList.contains("blog-page")) {
+      return "Todos con Miguel Carca\u00f1o - Windows Internet Explorer";
+    }
+
+    if (document.body.classList.contains("results-page")) {
+      return "blog apoyo Miguel Carca\u00f1o - The Searcher - Windows Internet Explorer";
+    }
+
+    return "The Searcher Espa\u00f1a - Windows Internet Explorer";
+  }
+
+  function getBrowserAddresses() {
+    var encodedQuery = encodeURIComponent(query || defaultResultsQuery);
+    var searchUrl = "http://thesearcher.es/search?q=" + encodedQuery;
+    var homeUrl = "http://thesearcher.es/";
+    var blogUrl = "http://apoyosamiguel.blogbikes.com/";
+
+    if (document.body.classList.contains("blog-page")) {
+      return [
+        blogUrl
+      ];
+    }
+
+    if (document.body.classList.contains("results-page")) {
+      return [
+        searchUrl,
+        "https://www.lavozdesevilla.es/sucesos/noticia3541.html",
+        "https://www.elheraldonacional.es/cronica/fuente.apx",
+        "https://www.magazinext.es/actualidad/article567.html",
+        blogUrl
+      ];
+    }
+
+    return [
+      homeUrl,
+      searchUrl
+    ];
+  }
+
+  function updateXpClock() {
+    var clock = document.querySelector("[data-xp-clock]");
+    var now;
+
+    if (!clock) {
+      return;
+    }
+
+    now = new Date();
+    clock.textContent = pad(now.getHours()) + ":" + pad(now.getMinutes());
+  }
+
+  function setupBrowserShell() {
+    var body = document.body;
+    var desktop;
+    var browserWindow;
+    var chrome;
+    var content;
+    var taskbar;
+    var scripts;
+    var nodes = [];
+    var child;
+    var addressNode;
+    var addresses;
+    var addressIndex = 0;
+    var taskLabel;
+
+    if (body.classList.contains("xp-shell-active")) {
+      return;
+    }
+
+    desktop = document.createElement("div");
+    browserWindow = document.createElement("section");
+    chrome = document.createElement("div");
+    content = document.createElement("div");
+    taskbar = document.createElement("footer");
+
+    desktop.className = "xp-desktop";
+    browserWindow.className = "ie-window";
+    browserWindow.setAttribute("aria-label", "Windows Internet Explorer");
+    chrome.className = "ie-chrome";
+    content.className = "browser-content";
+    taskbar.className = "xp-taskbar";
+
+    chrome.innerHTML =
+      '<div class="ie-titlebar">' +
+        '<span class="ie-title-icon">e</span>' +
+        '<span class="ie-title-text" data-browser-title></span>' +
+        '<span class="ie-window-controls"><button type="button">_</button><button type="button">[]</button><button type="button">X</button></span>' +
+      '</div>' +
+      '<div class="ie-menubar"><span>Archivo</span><span>Edici&oacute;n</span><span>Ver</span><span>Favoritos</span><span>Herramientas</span><span>Ayuda</span></div>' +
+      '<div class="ie-toolbar">' +
+        '<button type="button" class="ie-round-button">&lt;</button>' +
+        '<button type="button" class="ie-round-button muted">&gt;</button>' +
+        '<button type="button" class="ie-tool-button">Detener</button>' +
+        '<button type="button" class="ie-tool-button">Actualizar</button>' +
+        '<label class="ie-address-label">Direcci&oacute;n</label>' +
+        '<div class="ie-address-box"><span class="ie-address-icon">TS</span><span class="ie-address-text" data-browser-address></span></div>' +
+        '<button type="button" class="ie-go-button">Ir</button>' +
+      '</div>' +
+      '<div class="ie-linksbar"><span>Favoritos</span><a href="#">The Searcher</a><a href="#">Noticias</a><a href="#">Correo</a><a href="#">Blogbikes</a></div>';
+
+    taskbar.innerHTML =
+      '<button type="button" class="xp-start-button"><span class="xp-start-icon"></span><span>Inicio</span></button>' +
+      '<div class="xp-task-button"><span class="xp-task-icon">e</span><span data-xp-task-label></span></div>' +
+      '<div class="xp-task-spacer"></div>' +
+      '<div class="xp-tray"><span>ES</span><span class="xp-tray-light"></span><span data-xp-clock></span></div>';
+
+    scripts = body.querySelectorAll("script");
+    child = body.firstChild;
+    while (child) {
+      var next = child.nextSibling;
+      if (!(child.nodeType === 1 && child.tagName.toLowerCase() === "script")) {
+        nodes.push(child);
+      }
+      child = next;
+    }
+
+    for (var i = 0; i < nodes.length; i += 1) {
+      content.appendChild(nodes[i]);
+    }
+
+    browserWindow.appendChild(chrome);
+    browserWindow.appendChild(content);
+    desktop.appendChild(browserWindow);
+
+    body.insertBefore(desktop, scripts.length ? scripts[0] : null);
+    body.insertBefore(taskbar, scripts.length ? scripts[0] : null);
+    body.classList.add("xp-shell-active");
+
+    addressNode = body.querySelector("[data-browser-address]");
+    addresses = getBrowserAddresses();
+    taskLabel = body.querySelector("[data-xp-task-label]");
+    body.querySelector("[data-browser-title]").textContent = getBrowserTitle();
+
+    if (taskLabel) {
+      taskLabel.textContent = getBrowserTitle().replace(" - Windows Internet Explorer", "");
+    }
+
+    if (addressNode && addresses.length) {
+      addressNode.textContent = addresses[0];
+
+      if (addresses.length > 1) {
+        setInterval(function () {
+          addressIndex = (addressIndex + 1) % addresses.length;
+          addressNode.textContent = addresses[addressIndex];
+        }, 3200);
+      }
+    }
+
+    updateXpClock();
+    setInterval(updateXpClock, 30000);
+  }
+
   for (var i = 0; i < resultInputs.length; i += 1) {
     resultInputs[i].value = query;
   }
@@ -180,4 +334,5 @@
 
   setupBlogComments();
   setupVisitCounter();
+  setupBrowserShell();
 }());
